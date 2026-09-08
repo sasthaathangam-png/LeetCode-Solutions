e7373659181aa23423d758432aaa5ad0687c0786
Thangam S K <sasthaathangam@gmail.com>
2026-09-08 12:09:48 +0530

    Time: 5 ms (82.4%), Space: 46.1 MB (16.16%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+
+        int original = x;
+        int reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+
+            // Check for overflow (optional for LeetCode)
+            if (reversed > (Integer.MAX_VALUE - digit) / 10) {
+                return false;
+            }
+
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+        return reversed == original;
+        
+    }
+}
